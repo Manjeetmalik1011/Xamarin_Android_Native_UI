05d5c3fdb0d9d78a5386e04e3db518b01d1b5dcc
Manjeet Malik <manjeets@damcogroup.com>
2022-09-04 18:42:14 +0530

    intial commit

---
+++ ApplicationCore/Login/LoginActivity.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Xamarin_Android_Demo.ApplicationCore.Login
+{
+    [Activity(Theme = "@style/AppTheme", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
+    public class LoginActivity : Activity
+	{
+        EditText email;
+        EditText password;
+        protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+            SetContentView(Resource.Layout.Auth_Layout);
+
+            //Get email & password values from edit text  
+            //email = FindViewById<EditText>(Resource.Id.txtPassword);
+            //password = FindViewById<EditText>(Resource.Id.txtPassword);
+            //Trigger click event of Login Button  
+            //var button = FindViewById<FloatingActionButton>(Resource.Id.btnLogin);
+            //button.Click += DoLogin;
+        }
+        public void DoLogin(object sender, EventArgs e)
+        {
+            //NOTE: WE ARE CURRRENTLY GOING TO ADD STATIC CODE  
+            //IN MY PHASE () TWO I WILL CHANGE THIS CODE TO DYNAMIC   
+            //BECAUSE FOR DYNAMIC CODE WE NEED TO CRAETE WEB API FOR SAME  
+            //STATIC CODE FOR PHASE ONE ONLY. WE WILL CHANGE IT TO DYNAMIX IN PHASE TWO  
+            if (email.Text == "m10@gmail.com" && password.Text == "12345")
+            {
+                Toast.MakeText(this, "Login successfully done!", ToastLength.Long).Show();
+                StartActivity(typeof(MainActivity));
+            }
+            else
+            {
+                //Toast.makeText(getActivity(), "Wrong credentials found!", Toast.LENGTH_LONG).show();  
+                Toast.MakeText(this, "Wrong credentials found!", ToastLength.Long).Show();
+            }
+        }
+    }
+}
+
---
+++ MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Xamarin_Android_Demo.ApplicationCore.Login;
+
+namespace Xamarin_Android_Demo
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.MainLayout);
+            StartActivity(typeof(LoginActivity));
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
